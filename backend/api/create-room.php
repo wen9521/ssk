941383ff5deb5788855e9efd70fbd1dfe4a173c7
wen9521 <wenxiu9521@outlook.com>
2025-07-09 22:14:08 +0800

    Update create-room.php

--- backend/api/create-room.php
+++ backend/api/create-room.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: https://kk.wenge.ip-ddns.com');
+header('Access-Control-Allow-Origin: https://gewe.dpdns.org');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
