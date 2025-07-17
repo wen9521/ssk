68b511390c7a11b3c570466a9f16c38af025d04f
huangjie2599 <huangjie2599@gmail.com>
2025-07-17 03:54:41 +0000

    feat: 美化游戏大厅并适配手机端

--- frontend/src/pages/Lobby.js
+++ frontend/src/pages/Lobby.js
@@ -4,19 +4,20 @@ import { Link } from 'react-router-dom';
 function Lobby() {
   return (
     <div className="lobby">
-      <h1>游戏大厅</h1>
+      <h1>欢迎来到棋牌大厅</h1>
+      <p className="lobby-intro">选择您想玩的游戏，立即加入牌局！</p>
       <div className="game-list">
         <Link to="/thirteen-water" className="game-card">
           <h2>十三水</h2>
-          <p>经典福建扑克游戏</p>
+          <p>考验牌型组合与策略</p>
         </Link>
         <Link to="/doudizhu" className="game-card">
           <h2>斗地主</h2>
-          <p>三人对战，乐趣无穷</p>
+          <p>经典三人对抗，炸弹乐趣多</p>
         </Link>
         <Link to="/big-two" className="game-card">
           <h2>锄大地</h2>
-          <p>港式经典，步步为营</p>
+          <p>出牌技巧与团队配合</p>
         </Link>
       </div>
     </div>
