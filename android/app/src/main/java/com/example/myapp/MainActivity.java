7b592c09b3e022462557fa84b2c92800af77d84d
huangjie2599 <huangjie2599@gmail.com>
2025-07-18 07:00:01 +0000

    feat: Build native Android APK with integrated web frontend

---
+++ android/app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        
+        // Load the local HTML file from the assets folder
+        myWebView.loadUrl("file:///android_asset/index.html");
+    }
+}
