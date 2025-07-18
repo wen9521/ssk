5558d9f363f0640d5291a075aa20b2bbef7038a2
huangjie2599 <huangjie2599@gmail.com>
2025-07-18 14:31:15 +0000

    Fix: Add MultiDexApplication to support multidex

---
+++ android/app/src/main/java/com/example/myapp/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.myapp;
+
+import androidx.multidex.MultiDexApplication;
+
+public class MyApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
