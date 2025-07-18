6f967840a384e28a6e30fb95e06cfa9e823ee00f
huangjie2599 <huangjie2599@gmail.com>
2025-07-18 07:52:30 +0000

    fix(build): Rebuild Gradle Wrapper from official sources

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -25,6 +25,7 @@
 if "%OS%"=="Windows_NT" setlocal
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass any JVM options to this script.
+set DEFAULT_JVM_OPTS=
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
