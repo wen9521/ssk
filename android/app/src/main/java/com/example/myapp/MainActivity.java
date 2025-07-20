39e736d1f6ccdaeb2c7d087ee301c8afa8a7e20f
wen9521 <wenxiu9521@outlook.com>
2025-07-20 13:53:33 +0800

    Update MainActivity.java

--- android/app/src/main/java/com/example/myapp/MainActivity.java
+++ android/app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.myapp;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,14 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        myWebView = findViewById(R.id.webview);
-        MyApplication.initializeWebView(myWebView); // 使用 Application 类中的方法来初始化
 
-        // 添加 JavaScript 接口，命名为 "Android"
+        myWebView = findViewById(R.id.webview);
+
+        // 启用 JavaScript
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        // 保证跳转还在 WebView 内
+        myWebView.setWebViewClient(new WebViewClient());
+
+        // 添加 JS 接口
         myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
-        
-        myWebView.loadUrl("https://gewe.dpdns.org");
+
+        // 加载本地 assets 目录下的 index.html
+        myWebView.loadUrl("file:///android_asset/index.html");
     }
 
     @Override
@@ -36,24 +42,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * WebAppInterface 类允许 JavaScript 控制 Android 原生功能
-     */
     public class WebAppInterface {
         Activity activity;
 
-        // Instantiate the interface and set the context
         WebAppInterface(Activity activity) {
             this.activity = activity;
         }
 
-        // setOrientationToLandscape 方法
         @JavascriptInterface
         public void setOrientationToLandscape() {
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
 
-        /** 从 JavaScript 调用，将屏幕设置为竖屏 */
         @JavascriptInterface
         public void setOrientationToPortrait() {
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
