074085b01b5bc56ae70f9ae8dc5d8d946c5dec59
huangjie2599 <huangjie2599@gmail.com>
2025-07-24 03:57:11 +0000

    修复：修复了Android应用中扑克牌图片不显示的问题

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Card({ suit, rank }) {
-  const cardImagePath = `/assets/cards/${rank}_of_${suit}.svg`;
+  const cardImagePath = `assets/cards/${rank}_of_${suit}.svg`;
 
   return (
     <img 
