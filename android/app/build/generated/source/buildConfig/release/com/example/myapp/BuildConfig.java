a140c44de30cb7d96a661a14cb201d5c13a08d35
huangjie2599 <huangjie2599@gmail.com>
2025-07-26 16:11:32 +0000

    全面修复APK构建问题

---
+++ android/app/build/generated/source/buildConfig/release/com/example/myapp/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.myapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.myapp";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String WEB_ASSET_BASE = "file:///android_asset/www/";
+}
